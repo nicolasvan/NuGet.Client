ca8b7f98e3fd6bffb9b87ec0e1ef560b68e091f6
Justin Emgarten <justin@emgarten.com>
2017-03-17 00:04:42 -0700

    XPlat test support

--- build.sh
+++ build.sh
@@ -16,17 +16,17 @@ mkdir -p cli
 curl -o cli/dotnet-install.sh https://raw.githubusercontent.com/dotnet/cli/d2bbe1faa294012cec60b640e6522e0674224d3f/scripts/obtain/dotnet-install.sh
 
 # Download the CLI install script to cli test
-echo "Installing dotnet CLI test"
-mkdir -p cli_test
-curl -o cli_test/dotnet-install.sh https://raw.githubusercontent.com/dotnet/cli/d2bbe1faa294012cec60b640e6522e0674224d3f/scripts/obtain/dotnet-install.sh
+# echo "Installing dotnet CLI test"
+# mkdir -p cli_test
+# curl -o cli_test/dotnet-install.sh https://raw.githubusercontent.com/dotnet/cli/d2bbe1faa294012cec60b640e6522e0674224d3f/scripts/obtain/dotnet-install.sh
 
 # Run install.sh for cli
 chmod +x cli/dotnet-install.sh
 cli/dotnet-install.sh -i cli -c preview -v 1.0.1
 
 # Run install.sh fot cli test
-chmod +x cli_test/dotnet-install.sh
-cli_test/dotnet-install.sh -i cli_test -c preview -v 1.0.0-rc4-004788
+# chmod +x cli_test/dotnet-install.sh
+# cli_test/dotnet-install.sh -i cli_test -c preview -v 1.0.0-rc4-004788
 
 
 # Display current version
@@ -51,47 +51,15 @@ then
 fi
 
 # restore packages
-echo "$DOTNET restore src/NuGet.Core test/NuGet.Core.Tests --verbosity minimal"
-$DOTNET restore src/NuGet.Core test/NuGet.Core.Tests --verbosity minimal
+echo "$DOTNET msbuild build/build.proj /t:RestoreTests /p:VisualStudioVersion=15.0 /p:Configuration=Release /p:BuildNumber=1 /p:ReleaseLabel=beta"
+$DOTNET msbuild build/build.proj /t:Restore /p:VisualStudioVersion=15.0 /p:Configuration=Release /p:BuildNumber=1 /p:ReleaseLabel=beta
 if [ $? -ne 0 ]; then
 	echo "Restore failed!!"
 	exit 1
 fi
 
-# build xplat dll
-echo "$DOTNET build src/NuGet.Core/NuGet.CommandLine.XPlat --configuration release --framework netcoreapp1.0"
-$DOTNET build src/NuGet.Core/NuGet.CommandLine.XPlat --configuration release --framework netcoreapp1.0
-
 # run tests
-for testProject in `find test/NuGet.Core.Tests -type f -name project.json`
-do
-	testDir="$(pwd)/$(dirname $testProject)"
-
-	if grep -q "netcoreapp1.0" "$testProject"; then
-		pushd $testDir
-
-		case "$(uname -s)" in
-			Linux)
-				echo "$DOTNET test $testDir --configuration release --framework netcoreapp1.0 -notrait Platform=Windows -notrait Platform=Darwin"
-				$DOTNET test $testDir --configuration release --framework netcoreapp1.0 -notrait Platform=Windows -notrait Platform=Darwin
-				;;
-			Darwin)
-				echo "$DOTNET test $testDir --configuration release --framework netcoreapp1.0 -notrait Platform=Windows -notrait Platform=Linux"
-				$DOTNET test $testDir --configuration release --framework netcoreapp1.0 -notrait Platform=Windows -notrait Platform=Linux
-				;;
-			*) ;;
-		esac
-
-		if [ $? -ne 0 ]; then
-			echo "$testDir FAILED on CoreCLR"
-			RESULTCODE=1
-		fi
-
-		popd
-	else
-		echo "Skipping the tests in $testDir on CoreCLR"
-	fi
-
-done
+echo "$DOTNET msbuild build/build.proj /t:Test /p:VisualStudioVersion=15.0 /p:Configuration=Release /p:BuildNumber=1 /p:ReleaseLabel=beta"
+$DOTNET msbuild build/build.proj /t:Test /p:VisualStudioVersion=15.0 /p:Configuration=Release /p:BuildNumber=1 /p:ReleaseLabel=beta
 
 exit $RESULTCODE
